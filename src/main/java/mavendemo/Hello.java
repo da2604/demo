86a2aaa8acb5eec2fd6c06ca8262adaee64f6317
Disha <dishapatel2604@gmail.com>
2021-06-03 17:29:41 -0400

    first commit

---
+++ src/main/java/mavendemo/Hello.java
@@ -0,0 +1,10 @@
+package mavendemo;
+
+public class Hello {
+
+	public static void main(String[] args) {
+	  System.out.println("Hello...");
+
+	}
+
+}
---
+++ src/test/java/mavendemo/Test.java
@@ -0,0 +1,14 @@
+package mavendemo;
+
+import static org.junit.Assert.*;
+
+import org.junit.internal.JUnitSystem;
+
+public class Test {
+
+	@org.junit.Test
+	public void test() {
+		System.out.println(0);
+	}
+
+}
